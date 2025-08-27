f33c8ff06caafca72fa573010dc4985123d51270
Panthi14 <116938871+Parthrajsinh14@users.noreply.github.com>
2025-08-27 05:48:55 +0000

    comparison op

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,8 @@
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) // herefor ">=" first null will be converted to number and thanit becoms 1 so it will give output as true
+//undefine will always give false 
+
+
+// === it will check stricly with also dataType
+console.log("2" === 2);
