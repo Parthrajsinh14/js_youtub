9a79bca1698e1def192a23ea1db6da0d5fb68a7b
Panthi14 <116938871+Parthrajsinh14@users.noreply.github.com>
2025-08-27 04:23:27 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc" 
+
+
+console.log(typeof score);
+console.log(typeof (score));
+//string
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+//number
+console.log(valueInNumber);
+//NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1 ;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true 0=>false
+// "" =>fasle
+// "anyString" =>true
